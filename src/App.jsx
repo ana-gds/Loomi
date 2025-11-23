210d210ea6fff06babf6e54286702accd5bcf9f1
Carol <guedescarolina24@gmail.com>
2025-11-23 19:51:04 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/Button/Button.jsx';
+
+function App() {
+
+
+  return (
+    <>
+    <Button
+        label="Favoritos"
+    />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ui/Button/Button.jsx
@@ -0,0 +1,19 @@
+export function Button({
+    label,
+    onClick,
+    type = 'button',
+    ClassNames = '',
+    ...rest
+}) {
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            className={`text-base bg-gradient-to-t from-principal to-gradiente text-fundo 
+            rounded-xl shadow-sm shadow-color-texto-principal px-4 py-2 ${ClassNames}`}
+            {...rest}
+        >
+            {label}
+        </button>
+    );
+}
